7f1baf051a4557b2b962e4309a8e6bea456fb468
parabavire <jdparabavire@gmail.com>
2025-06-28 17:19:21 -0400

    post create

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
@@ -10,3 +11,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    # get_absolute_url method to redirect after creation
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
